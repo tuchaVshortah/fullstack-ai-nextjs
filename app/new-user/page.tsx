926f831d998349fc16f3f64780fe4ad1c453cf78
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-14 22:45:22 +0500

    create a new user setup page

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,29 @@
-const NewUser = () => {
-  return <div>hi</div>
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  const match = prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  })
+
+  if (!match) {
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    })
+  }
+  redirect('/journal')
+}
+
+const NewUser = async () => {
+  await createNewUser()
+  return <div>I guess you might be a new user huh?..</div>
 }
 
 export default NewUser
