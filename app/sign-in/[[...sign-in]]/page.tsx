dc0129b67f5570ad1b2ce5e8710adbf449d8d304
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-14 20:38:40 +0500

    force redirects after sign in and sign up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => {
-  return <SignIn />
+  return <SignIn forceRedirectUrl={'/journal'} />
 }
 
 export default SignInPage
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignUp } from '@clerk/nextjs'
 
 const SignUpPage = () => {
-  return <SignUp />
+  return <SignUp forceRedirectUrl={'/new-user'} />
 }
 
 export default SignUpPage
