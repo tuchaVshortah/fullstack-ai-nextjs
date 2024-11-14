99486ad33353e23b00270da716c118871bfd493c
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-14 22:10:07 +0500

    dynamically change hrefs depending on the state of auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import { auth } from '@clerk/nextjs/server'
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth()
+  let href = userId ? '/journal' : '/new-user'
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -11,7 +15,7 @@ const Home = () => {
           you have to do is be honest.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
