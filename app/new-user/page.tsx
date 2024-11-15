38394148699c5458a1f8bd206d27f59891e09b50
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-15 13:57:26 +0500

    fix not awaiting async method

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 
 const createNewUser = async () => {
   const user = await currentUser()
-  const match = prisma.user.findUnique({
+  const match = await prisma.user.findUnique({
     where: {
       clerkId: user?.id as string,
     },
