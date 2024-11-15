2a0b5b42339ac3098cd451f1ee54423003ac9181
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-15 17:54:38 +0500

    get the user's journal entries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { prisma } from '@/utils/db'
+import { getUserByClerkID } from '@/utils/auth'
 
-const JournalPage = () => {
+const getEntries = async () => {
+  const user = await getUserByClerkID()
+  const entries = prisma.journalEntry.findMany({
+    where: {
+      userId: user.id as unknown as string,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries', entries)
   return <div>JournalPage</div>
 }
 
