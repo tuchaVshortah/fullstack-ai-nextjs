adf1df96a28b6fc01b92eceec2ae935f77e2ac6c
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-15 14:36:41 +0500

    create a basic layout skeleton with a sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <div>DashboardLayout</div>
-      <div>{children}</div>
+    <div className="w-screen h-screen relative">
+      <aside className="absolute top-0 left-0 h-full w-[200px] border border-r border-black/10">
+        Mood
+      </aside>
+      <div className="ml-[200px]">
+        <header className="relative h-[60px] border border-b border-black/10">
+          hello
+        </header>
+        <div>{children}</div>
+      </div>
     </div>
   )
 }
