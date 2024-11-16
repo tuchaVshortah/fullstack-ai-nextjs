b29f7311014824e328882b0bbf68d5e904743621
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-16 10:41:01 +0500

    use new components

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { prisma } from '@/utils/db'
 import { getUserByClerkID } from '@/utils/auth'
+import NewEntryCard from '@/components/NewEntryCard'
+import EntryCard from '@/components/EntryCard'
 
 const getEntries = async () => {
   const user = await getUserByClerkID()
@@ -19,7 +21,14 @@ const getEntries = async () => {
 const JournalPage = async () => {
   const entries = await getEntries()
   console.log('entries', entries)
-  return <div>JournalPage</div>
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <NewEntryCard />
+      {entries.map((entry) => (
+        <EntryCard key={entry.id} entry={entry} />
+      ))}
+    </div>
+  )
 }
 
 export default JournalPage
