fd2c06df20acdd3eea5f9bac13fde2cd7b4aa96f
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-16 10:50:36 +0500

    set the height for children taking into account the height of the navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,13 +7,13 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <aside className="absolute top-0 left-0 h-full w-[200px] border border-r border-black/10">
         Mood
       </aside>
-      <div className="ml-[200px]">
+      <div className="ml-[200px] h-full">
         <header className="relative h-[60px] border border-b border-black/10">
           <div className="h-full w-full px-6 flex items-center justify-end">
             <UserButton />
           </div>
         </header>
-        <div>{children}</div>
+        <div className="h-[calc(100vh-60px)]">{children}</div>
       </div>
     </div>
   )
