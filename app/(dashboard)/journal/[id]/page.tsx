c15752fd65f15c35c6003d9b082363514f0abe36
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-17 12:32:47 +0500

    get the entry from the db and show the contents

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,7 +1,30 @@
+import Editor from '@/components/Editor'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
 import React from 'react'
 
+const getEntry = async (id) => {
+  const user = await getUserByClerkID()
+  const entry = prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: id,
+      },
+    },
+  })
+
+  return entry
+}
+
 const EntryPage = async ({ params }) => {
-  return <div>{params.id}</div>
+  const entry = await getEntry(params.id)
+
+  return (
+    <div>
+      <Editor entry={entry} />
+    </div>
+  )
 }
 
 export default EntryPage
