5d15a9fc97429d62a6fb7bc1d4d5bee92969286e
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-17 12:10:56 +0500

    show summary and mood placeholders

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -3,8 +3,8 @@ const EntryCard = ({ entry }) => {
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
       <div className="px-4 py-5">{date}</div>
-      {/* <div className="px-4 py-5">{entry.analysis.summary}</div>
-      <div className="px-4 py-4">{entry.analysis.mood}</div> */}
+      <div className="px-4 py-5">summary</div>
+      <div className="px-4 py-4">mood</div>
     </div>
   )
 }
