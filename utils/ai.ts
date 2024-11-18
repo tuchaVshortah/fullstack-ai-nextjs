5a019f0ee986fa7711337b7cf5c888359831a02e
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-18 22:59:17 +0500

    use zod and StructuredOutputParser

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,4 +1,28 @@
 import { ChatOpenAI } from '@langchain/openai'
+import { StructuredOutputParser } from '@langchain/core/output_parsers'
+import z from 'zod'
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe('The mood of the person who wrote the journal entry.'),
+    summary: z.string().describe('Quick summary of the entire entry.'),
+    negative: z
+      .boolean()
+      .describe('Whether the journal entry has a negative effect or outcome.'),
+    color: z
+      .string()
+      .describe(
+        'A hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+      ),
+    sentimentScore: z
+      .number()
+      .describe(
+        'Sentiment of the text and rated on a scale from -10 to 10, where -10 is extremely negative, 0 is neutral, and 10 is extremely positive.'
+      ),
+  })
+)
 
 export const analyze = async (prompt) => {
   const model = new ChatOpenAI({
