157b0fada5569a6e95d5e416ee6770e263f1950f
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-18 15:58:50 +0500

    add analysis sidebar

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -20,10 +20,35 @@ const getEntry = async (id) => {
 const EntryPage = async ({ params }) => {
   params = await params
   const entry = await getEntry(params.id)
-
+  const analysisData = [
+    { name: 'Subject', value: '' },
+    { name: 'Summary', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: 'false' },
+  ]
   return (
-    <div>
-      <Editor entry={entry} />
+    <div className="grid grid-cols-3 h-full w-full">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="flex items-center justify-between px-2 py-4 border-b border-black/10"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
