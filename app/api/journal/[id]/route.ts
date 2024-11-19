37119e1691b1805a4dbb269189f52004e8fd109d
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-19 17:29:56 +0500

    analyze the patched journal entry

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { NextResponse } from 'next/server'
@@ -15,6 +16,15 @@ export const PATCH = async (request: Request, { params }) => {
     },
     data: {
       content,
+    },
+  })
+
+  await prisma.analysis.update({
+    where: {
+      entryId: updatedEntry.id,
+    },
+    data: {
+      ...(await analyze(updatedEntry.content)),
     },
   })
 
