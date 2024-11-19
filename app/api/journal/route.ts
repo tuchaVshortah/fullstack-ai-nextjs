ba51f516fdb93d9e4d8fdddc4bf7d720342fcdff
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-19 16:16:28 +0500

    analyze the entry upon creation

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -2,6 +2,7 @@ import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
+import { analyze } from '@/utils/ai'
 
 export const POST = async (request: Request) => {
   const user = await getUserByClerkID()
@@ -9,6 +10,14 @@ export const POST = async (request: Request) => {
     data: {
       userId: user.id as string,
       content: 'Write about your day!',
+    },
+  })
+
+  const analysis = await analyze(entry.content)
+  await prisma.analysis.create({
+    data: {
+      entryId: entry.id,
+      ...analysis,
     },
   })
 
