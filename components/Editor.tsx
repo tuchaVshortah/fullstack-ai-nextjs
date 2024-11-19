be9ec80d3976c72e127abf608b7546d3664ab42a
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-19 18:55:49 +0500

    move the analysis side bar to the Editor client component

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -6,6 +6,15 @@ import { useAutosave } from 'react-autosave'
 const Editor = ({ entry }) => {
   const [value, setValue] = useState(entry.content)
   const [isLoading, setLoading] = useState(false)
+
+  const { mood, summary, color, subject, negative } = entry?.analysis
+  const analysisData = [
+    { name: 'Subject', value: subject },
+    { name: 'Summary', value: summary },
+    { name: 'Mood', value: mood },
+    { name: 'Negative', value: negative ? 'True' : 'False' },
+  ]
+
   useAutosave({
     data: value,
     onSave: async (_value) => {
@@ -16,13 +25,33 @@ const Editor = ({ entry }) => {
   })
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full grid grid-cols-3">
       {isLoading && <div>...loading</div>}
-      <textarea
-        className="w-full h-full p-8 text-xl outline-none"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      ></textarea>
+      <div className="col-span-2">
+        <textarea
+          className="w-full h-full p-8 text-xl outline-none"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="border-l border-black/10">
+        <div className="px-6 py-10" style={{ backgroundColor: color }}>
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="flex items-center justify-between px-2 py-4 border-b border-black/10"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
