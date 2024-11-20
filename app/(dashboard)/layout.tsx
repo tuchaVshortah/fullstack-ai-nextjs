609119e01cbd5ea1e35dc04459746c2bbc00c9aa
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-20 11:18:45 +0500

    add navigation links to the left sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,24 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 import React from 'react'
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+]
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-screen h-screen relative">
       <aside className="absolute top-0 left-0 h-full w-[200px] border border-r border-black/10">
-        Mood
+        <div>Mood</div>
+        <ul>
+          {links.map((link) => (
+            <li key={link.href} className="px-2 py-6 text-xl">
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
       </aside>
       <div className="ml-[200px] h-full">
         <header className="relative h-[60px] border border-b border-black/10">
