7728169a9ee060600ebc3a918e92fccc3594778b
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-20 11:36:11 +0500

    use the question component

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -4,6 +4,7 @@ import { getUserByClerkID } from '@/utils/auth'
 import NewEntryCard from '@/components/NewEntryCard'
 import EntryCard from '@/components/EntryCard'
 import Link from 'next/link'
+import Question from '@/components/Question'
 
 const getEntries = async () => {
   const user = await getUserByClerkID()
@@ -25,6 +26,9 @@ const JournalPage = async () => {
   return (
     <div className="p-10 bg-zinc-400/10 h-full">
       <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="my-8">
+        <Question />
+      </div>
       <div className="grid grid-cols-3 gap-4">
         <NewEntryCard />
         {entries.map((entry) => (
