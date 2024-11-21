ee8d7248654940083fc1c215157383dfd6f24c54
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-21 13:26:40 +0500

    add missing user.id

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,6 +1,10 @@
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
 import React from 'react'
 
-const History = () => {
+const History = async () => {
+  const user = await getUserByClerkID()
+  const analyses = await prisma.analysis.findMany({})
   return <div>History</div>
 }
 
