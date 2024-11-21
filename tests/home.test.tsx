b8bffd939f96957ed767abba4f7de47b2e4a65c1
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-21 20:41:24 +0500

    mock clerk autht to pass the test

--- tests/home.test.tsx
+++ tests/home.test.tsx
@@ -5,7 +5,7 @@ import Home from '../app/page'
 vi.mock('@clerk/nextjs', () => {
   // Create an mockedFunctions object to match the functions we are importing from the @nextjs/clerk package in the ClerkComponent component.
   const mockedFunctions = {
-    auth: () =>
+    auth: async () =>
       new Promise((resolve) =>
         resolve({ userId: 'user_2NNEqL2nrIRdJ194ndJqAHwEfxC' })
       ),
@@ -22,13 +22,44 @@ vi.mock('@clerk/nextjs', () => {
   return mockedFunctions
 })
 
+vi.mock('@clerk/nextjs/server', async () => {
+  return {
+    auth: async () => {
+      const object = {
+        sessionId: 'sess_2GaMqUCB3Sc1WNAkWuNzsnYVVEy',
+        userId: 'user_2F2u1wtUyUlxKgFkKqtJNtpJJWj',
+        orgId: null,
+        orgRole: null,
+        orgSlug: null,
+        orgPermissions: null,
+        has: function () {},
+        getToken: async function () {},
+        claims: {
+          azp: 'http://localhost:3000',
+          exp: 1666622607,
+          iat: 1666622547,
+          iss: 'https://clerk.quiet.muskox-85.lcl.dev',
+          nbf: 1666622537,
+          sid: 'sess_2GaMqUCB3Sc1WNAkWuNzsnYVVEy',
+          sub: 'user_2F2u1wtUyUlxKgFkKqtJNtpJJWj',
+        },
+      }
+      return object
+    },
+  }
+})
+
 vi.mock('next/font/google', () => {
   return {
     Inter: () => ({ className: 'inter' }),
   }
 })
 
-test(`Home`, async () => {
+vi.mock('server-only', () => {
+  return {}
+})
+
+test('Home', async () => {
   render(await Home())
   expect(screen.getByText('The best Journal app, period.')).toBeTruthy()
 })
