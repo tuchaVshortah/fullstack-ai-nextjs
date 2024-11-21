d2e17edd46fbcc06a3c4f641361d33127552084e
Nurkanat Baisenkul <nukenbai190104@gmail.com>
2024-11-21 17:15:06 +0500

    output the average sentiment score across all journal entries

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -2,10 +2,26 @@ import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import React from 'react'
 
-const History = async () => {
+const getData = async () => {
   const user = await getUserByClerkID()
-  const analyses = await prisma.analysis.findMany({})
-  return <div>History</div>
+  const analyses = await prisma.analysis.findMany({
+    where: {
+      userId: user.id,
+    },
+    select: {
+      sentimentScore: true,
+    },
+  })
+
+  const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+  const avg = Math.round(sum / analyses.length)
+  return { analyses, avg }
+}
+
+const History = async () => {
+  const { analyses, avg } = await getData()
+
+  return <div>history: {avg}</div>
 }
 
 export default History
